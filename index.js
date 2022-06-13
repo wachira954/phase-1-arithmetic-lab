cd39418848ba5ac4c6487b60037c580975e536ee
wachira <wachiraa954@gmail.com>
2022-06-13 13:02:27 +0300

    commit tree 50eef77bde8b78e3cf4cf7eb470e97015e951b34

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2
 
-// Write your code here
+const random = math.floor(math.random() * 1000) + 1;
+
+const num3 = 8;
+const num4 = 2;
+const mod = num3 % num4;
+
+
+const max =math.max (1,2,3,4,5,6,7,8,9,20);
